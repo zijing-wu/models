3808da185f8765310e7d4dfd27165bee506181cb
haoyu9112 <haoyu9112@gmail.com>
2018-03-22 16:07:56 -0400

    update

--- research/delf/delf/python/examples/gen_features.sh
+++ research/delf/delf/python/examples/gen_features.sh
@@ -3,5 +3,5 @@
 
 export PYTHONPATH=$PYTHONPATH:/Users/haoyuhe/github/google_landmark/models/research:/Users/haoyuhe/github/google_landmark/models/research/slim
 
-python extract_features.py --list_images_path all_files.txt --output_dir all_features
+python3 extract_features.py --list_images_path all_files.txt --output_dir all_features
 
