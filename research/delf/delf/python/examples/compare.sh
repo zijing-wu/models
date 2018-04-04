8156feab5d1e442240a01acb0b1867d9d5201c86
haoyu9112 <haoyu9112@gmail.com>
2018-04-04 18:49:14 -0400

    update

--- research/delf/delf/python/examples/compare.sh
+++ research/delf/delf/python/examples/compare.sh
@@ -10,14 +10,14 @@
 #python3 -m cProfile -o new.profile match_images_all.py \
 #  test_features/ test_features/ 0 20 10 lines_out/
 
-CUDA_VISIBLE_DEVICES=0 nohup python3 match_images_all.py train_features/ test_features/ 0 14468 1000 lines_out/ > gpu0.log &
-CUDA_VISIBLE_DEVICES=1 nohup python3 match_images_all.py train_features/ test_features/ 14468 28936 1000 lines_out/ > gpu1.log &
-CUDA_VISIBLE_DEVICES=2 nohup python3 match_images_all.py train_features/ test_features/ 28936 43405 1000 lines_out/ > gpu2.log &
-CUDA_VISIBLE_DEVICES=3 nohup python3 match_images_all.py train_features/ test_features/ 43405 57873 1000 lines_out/ > gpu3.log &
-CUDA_VISIBLE_DEVICES=4 nohup python3 match_images_all.py train_features/ test_features/ 57873 72341 1000 lines_out/ > gpu4.log &
-CUDA_VISIBLE_DEVICES=5 nohup python3 match_images_all.py train_features/ test_features/ 72341 86810 1000 lines_out/ > gpu5.log &
-CUDA_VISIBLE_DEVICES=6 nohup python3 match_images_all.py train_features/ test_features/ 86810 101278 1000 lines_out/ > gpu6.log &
-CUDA_VISIBLE_DEVICES=7 nohup python3 match_images_all.py train_features/ test_features/ 101278 115747 1000 lines_out/ > gpu7.log &
+CUDA_VISIBLE_DEVICES=0 nohup python3 match_images_all.py train_features/ test_features/ 0 14468 1000 lines_out/ > lgpu0.log &
+CUDA_VISIBLE_DEVICES=1 nohup python3 match_images_all.py train_features/ test_features/ 14468 28936 1000 lines_out/ > lgpu1.log &
+CUDA_VISIBLE_DEVICES=2 nohup python3 match_images_all.py train_features/ test_features/ 28936 43405 1000 lines_out/ > lgpu2.log &
+CUDA_VISIBLE_DEVICES=3 nohup python3 match_images_all.py train_features/ test_features/ 43405 57873 1000 lines_out/ > lgpu3.log &
+CUDA_VISIBLE_DEVICES=4 nohup python3 match_images_all.py train_features/ test_features/ 57873 72341 1000 lines_out/ > lgpu4.log &
+CUDA_VISIBLE_DEVICES=5 nohup python3 match_images_all.py train_features/ test_features/ 72341 86810 1000 lines_out/ > lgpu5.log &
+CUDA_VISIBLE_DEVICES=6 nohup python3 match_images_all.py train_features/ test_features/ 86810 101278 1000 lines_out/ > lgpu6.log &
+CUDA_VISIBLE_DEVICES=7 nohup python3 match_images_all.py train_features/ test_features/ 101278 115747 1000 lines_out/ > lgpu7.log &
 
 
 
