116d5aa628fa3f11a5a1e9e983b005a1ee258ae0
yogurtkun <yogurtkun@126.com>
2018-04-05 16:15:25 -0400

    change gen_list.py

--- research/delf/delf/python/examples/gen_list.py
+++ research/delf/delf/python/examples/gen_list.py
@@ -6,9 +6,12 @@ label2id = {}
 with open('data_retrieval/train.csv') as file:
     csv_reader =  csv.reader(file)
     for row in csv_reader:
-         if row[2] not in label2id and os.path.exists(os.path.join('train_features_ds2',row[0]+'.delf')):
-             label2id[row[2]] = row[0]
+         if os.path.exists(os.path.join('train_features_ds2',row[0]+'.delf')):
+             if row[2] not in label2id:
+                 label2id[row[2]] = []
+             label2id[row[2]].append([row[0]])
 
 with open('./image_list.txt','w') as file:
     for k,v in label2id.items():
-        file.write(v+','+k+'\n')
+        if len(v) > 10:
+            file.write(v[0]+','+k+'\n')
