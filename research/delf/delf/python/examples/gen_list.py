db792818f16b78b18df215d48d4ba65581ba7937
yogurtkun <yogurtkun@126.com>
2018-04-05 12:32:46 -0400

    generate the list

---
+++ research/delf/delf/python/examples/gen_list.py
@@ -0,0 +1,13 @@
+import os,csv
+
+count = 1
+label2id = {}
+with open('data_retrieval/train.csv') as file:
+    csv_reader =  csv.reader(file)
+    for row in csv_reader:
+         if row[2] not in label2id and os.path.exists(os.path.join('train_features',row[0]+'.delf')):
+             label2id[row[2]] = row[0]
+
+with open('./image_list.txt','w') as file:
+    for k,v in label2id.items():
+        file.write(v+','+k+'\n')
